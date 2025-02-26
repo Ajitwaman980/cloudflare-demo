05096541591b7aace619a5029affca9a51758a91
ajit waman <ajitwaman353@gmail.com>
2025-02-27 00:22:25 +0530

    cloudflare demo

---
+++ demo-cloudflare/src/index.ts
@@ -0,0 +1,8 @@
+export default {
+	async fetch(request, env, ctx) {
+		return Response.json({
+			messagre: 'This is a test request and will be sent to the server  ',
+			request: request.url,
+		});
+	},
+} satisfies ExportedHandler<Env>;
